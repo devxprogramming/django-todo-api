964935d11597ab6e7b1e5b540fae17c56fd7afed
Hunter Britt <devxprogramming@gmail.com>
2025-01-07 17:59:34 +0000

    Deployment phase

--- backend/apps/DjTodo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import DjTodo
-
-admin.site.register(DjTodo)
--- backend/apps/DjTodo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjtodoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.DjTodo'
